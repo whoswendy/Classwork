3e76456064beffcc4e9502d32646b3e17f9727f9
whoswendy <whu9517@bths.edu>
2016-09-14 13:56:25 -0400

    Experiment Bye Bye

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -4,6 +4,7 @@ public class ProceduralExample {
   public static void main(String[]args){
     System.out.println("Hello World");
     System.out.println("Bye World");
+    System.out.println("Bye Bye");
   }
 
 }
