481f5e981d9f2dbbf07f49ceddec48ab69fcfd6f
Student6 <Student6@10.8.33.148>
2016-09-14 13:51:39 -0400

    Experiment?

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -3,6 +3,7 @@ package introduction;
 public class ProceduralExample {
   public static void main(String[]args){
     System.out.println("Hello World");
+    System.out.println("Bye World");
   }
 
 }
