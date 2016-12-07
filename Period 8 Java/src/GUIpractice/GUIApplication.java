9040ef40a260c47ec1912b0a07974c61be51f1c0
Student6 <Student6@10.8.33.148>
2016-12-07 14:03:03 -0500

    GIU SCREEN

--- Period 8 Java/src/GUIpractice/GUIApplication.java
+++ Period 8 Java/src/GUIpractice/GUIApplication.java
@@ -1,8 +1,13 @@
 package GUIpractice;
 
+import java.awt.Graphics;
+
 import javax.swing.JFrame;
 
 public class GUIApplication extends JFrame{
+	
+	private Screen currentScreen;
+	
 	
 	
 	public static void main(String[] args) 
@@ -20,6 +25,17 @@ public class GUIApplication extends JFrame{
 		int width = 600;
 		int height = 400;
 		setBounds(x,y,width,height);
+		initScreen();
 		setVisible(true);
+	}
+
+	protected void initScreen() {
+		Screen startScreen = new Screen(getWidth(),getHeight());
+		currentScreen = startScreen;
+	}
+	
+	public void paint(Graphics g)
+	{
+		g.drawImage(currentScreen.getImage(), 0, 0, null);
 	}
 }
--- Period 8 Java/src/GUIpractice/Screen.java
+++ Period 8 Java/src/GUIpractice/Screen.java
@@ -1,5 +1,36 @@
 package GUIpractice;
 
+import java.awt.image.BufferedImage;
+
 public class Screen {
+	
+	private int width;
+	private int height;
+	protected BufferedImage image;//buffer image data thats tells you how pixels are displayed
+	//protected similar to private but accessible to subclasses
+	
+	public Screen(int width, int height)
+	{
+		this.width = width;
+		this.height = height;
+		initImage();
+		
+	}
+
+	private void initImage() {
+		image = new BufferedImage(width,height,BufferedImage.TYPE_INT_ARGB);//red green blue and transparency (alpha)
+		update();
+		
+	}
+
+	public void update() {
+		//this is where you draw stuff
+		
+	}
+	
+	public BufferedImage getImage()
+	{
+		return image;
+	}
 
 }
