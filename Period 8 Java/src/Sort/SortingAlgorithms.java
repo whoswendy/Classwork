f021ee3d3b53b256f5e85f9f00f792a816264c49
Student6 <Student6@10.8.33.148>
2016-12-08 13:35:11 -0500

    MORE SORT

--- Period 8 Java/src/Sort/SortingAlgorithms.java
+++ Period 8 Java/src/Sort/SortingAlgorithms.java
@@ -13,15 +13,18 @@ public class SortingAlgorithms {
 		bubbleSort(copy(arrayToSort));
 	}
 	
-	public static int[] copy(int[] arr){
+	public static int[] copy(int[] arr)
+	{
 		int[] copy = new int[arr.length];
-		for(int i = 0; i < copy.length; i++){
+		for(int i = 0; i < copy.length; i++)
+		{
 			copy[i]=arr[i];
 		}
 		return copy;
 	}
 	
-	public static void swap(int[] arr, int i, int j){
+	public static void swap(int[] arr, int i, int j)
+	{
 		System.out.println("Swapping "+arr[i]+" and "+arr[j]);
 		int placeHolder=arr[j];
 		arr[j]=arr[i];
@@ -29,14 +32,18 @@ public class SortingAlgorithms {
 		System.out.println(Arrays.toString(arr));
 	}
 	
-	public static void selectionSort(int[] arrayToSort){
+	public static void selectionSort(int[] arrayToSort)
+	{
 		int minIndex=0;
 		int minimum=arrayToSort[0];
-		for(int i=0;i<arrayToSort.length;i++){
+		for(int i=0;i<arrayToSort.length;i++)
+		{
 			minIndex=i;
 			minimum=arrayToSort[i];
-			for(int j=i;j<arrayToSort.length;j++){
-				if(arrayToSort[j]<minimum){
+			for(int j=i;j<arrayToSort.length;j++)
+			{
+				if(arrayToSort[j]<minimum)
+				{
 					minimum=arrayToSort[j];
 					minIndex=j;
 				}
@@ -53,11 +60,9 @@ public class SortingAlgorithms {
 			 {
 				 if(arrayToSort[j]>arrayToSort[j+1])
 				 {
-					 swap(arrayToSort,j,j+1);
-					 
+					 swap(arrayToSort,j,j+1); 
 				 }
 			 }
-			
 		}
 	}
 	
@@ -98,5 +103,91 @@ public class SortingAlgorithms {
 		 }
 		 	return merge;
 	}
+	
+	public static void insertionSort(int[] arrayToSort){
+		//this algorithm takes one element at a time to 
+		//add in sorted order...
+		//start at 1 since the element at zero is
+		//already in "sorted order" (since it is alone)
+		for (int i = 1; i < arrayToSort.length; i++) {
+			//with each element, i, go BACKWARDS from i, 
+			//until you find a value that is smaller
+			//swap as you go
+			for(int j = i ; j > 0 ; j--){
+				if(arrayToSort[j] < arrayToSort[j-1]){
+					swap(arrayToSort,j,j-1);
+				}
+			}
+		}
+	}
+
+	//note: merge sort is not an "in-place" algorithm. It actually makes copies of the initial array
+	//so this is the only method that actual has a return type
+	public static int[] mergeSort(int[] arrayToSort){
+		int n = arrayToSort.length;
+
+		if(n<=1){
+			//trivial sort when only one (or no) element
+			//is in the array
+			return arrayToSort;
+		}else{	
+			//split the array in half
+			int[] partA = new int[n/2];
+			//be careful of rounding!
+			int[] partB = new int[n-n/2];
+			for(int i=0; i<n; i++){
+				if(i<partA.length){
+					partA[i]=arrayToSort[i];
+				}else{
+					partB[i-partA.length]=arrayToSort[i];
+				}
+			}
+			return merge(mergeSort(partA),mergeSort(partB));
+		}
+	}
+	
+	
+	//since quickSort is recursive, the start and end must be specified 
+	//(to break it down through recursive calls)
+	public static void quickSort(int[] arrayToSort, int start, int end)
+	{
+		//trivial case is when the array is empty or contains one element
+		//sort only if this is not the case, (otherwise nothing is done)
+		if(start<end){			
+			//choose the pivot from the end
+			int pivot = arrayToSort[end];
+			System.out.println("PIVOT "+pivot);
+			//pIndex is the index where the pivot will go at the end.
+			//It is also the number of elements SMALLER than the pivot
+			//(note: pivot cannot be smaller than start, since this is only
+			//sorting elements after start)
+			int pIndex = start;
+			//compare every element to the pivot
+			for(int i=start; i<end; i++){
+				if(arrayToSort[i]<pivot){
+					swap(arrayToSort,i,pIndex);
+					//pIndex increases because there is another element smaller than the pivot
+					pIndex++;
+				}
+			}
+			//now put the pivot in the correct spot
+			swap(arrayToSort,pIndex, end);
+			//and sort the elements on the left and right of the pivot
+			if(start<pIndex-1)
+			{
+				quickSort(arrayToSort, start, pIndex-1);
+			}	
+			if(pIndex-1 < end)
+			{
+				quickSort(arrayToSort, pIndex+1, end);
+			}
+		}
+	}
+	
+	//AP required sorting algorithms
+	//selection sort
+	//insertion sort
+	//merge sort
+	//binary search
 
 }
