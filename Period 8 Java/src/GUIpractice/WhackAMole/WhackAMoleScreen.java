64f3f93cfa6cff942b172717700e342f2eeb44b2
Student6 <Student6@10.8.33.148>
2016-12-22 14:02:39 -0500

    s

--- Period 8 Java/src/GUIpractice/WhackAMole/WhackAMoleScreen.java
+++ Period 8 Java/src/GUIpractice/WhackAMole/WhackAMoleScreen.java
@@ -17,6 +17,7 @@ public class WhackAMoleScreen extends ClickableScreen {
 	public WhackAMoleScreen(int width, int height) {
 		super(width, height);
 		// TODO Auto-generated constructor stub
+		timeLeft = 60.0;
 	}
 
 	@Override
